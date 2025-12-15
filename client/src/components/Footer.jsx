4b0854ea360468a87fe3b2b763635c0a5d357302
TVATDCI <tvatdci@gmail.com>
2025-12-15 04:56:59 +0100

    Update UI bg

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <footer className="bg-gray-900 text-gray-400 py-6">
-      <div className="container mx-auto text-center">
-        <p>&copy; {year} Your Name. All Rights Reserved.</p>
-        <p className="mt-2">Built with React, Tailwind CSS, and Framer Motion.</p>
+    <footer className='bg-black text-gray-400 py-6'>
+      <div className='container mx-auto text-center'>
+        <p>&copy; {year} TNTHNGVDYNND. All Rights Reserved.</p>
+        <p className='mt-2'>
+          Built with React, Tailwind CSS, and Framer Motion.
+        </p>
       </div>
     </footer>
   );
