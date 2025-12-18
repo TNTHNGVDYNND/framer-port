fe16093e195669828d2e4b7417b582dcab6b2fb3
TVATDCI <tvatdci@gmail.com>
2025-12-18 03:41:10 +0100

    import theme route

--- client/src/AppRoutes.jsx
+++ client/src/AppRoutes.jsx
@@ -6,20 +6,21 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import Work from './pages/Work';
 import About from './pages/About';
-import Contact from './pages/Contact'; // Import the Contact page
+import Contact from './pages/Contact';
+import ThemeCard from './pages/ThemeCard';
 
 const AppRoutes = () => {
   const location = useLocation();
 
   return (
-    <AnimatePresence mode="wait">
+    <AnimatePresence mode='wait'>
       <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Layout />}>
+        <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="work" element={<Work />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} /> {/* Add the Contact route */}
-          {/* Add other routes here */}
+          <Route path='work' element={<Work />} />
+          <Route path='about' element={<About />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='theme' element={<ThemeCard />} />
         </Route>
       </Routes>
     </AnimatePresence>
