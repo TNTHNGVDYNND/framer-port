e352c69b21412aa369834a1d39006aca5d0f97d0
TVATDCI <tvatdci@gmail.com>
2025-12-19 19:58:26 +0100

    Layout-changed bg color for testing

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -7,7 +7,7 @@ import { ReactLenis } from 'lenis/react';
 const Layout = () => {
   return (
     <ReactLenis root>
-      <div className='bg-gray-900 min-h-screen flex flex-col'>
+      <div className='bg-neutral-500 min-h-screen flex flex-col'>
         <Navbar />
         <main className='grow'>
           <Outlet />
