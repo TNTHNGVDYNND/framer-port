ad75e4b9ef729e06510e7440c1e5531176e0cbda
TNTHNGVDYNND <tuanthong.vaidyanond@gmail.com>
2026-02-12 21:39:52 +0100

    refactor(components): update Layout to accommodate sidebar and new components
    - Add ml-16 margin to main content for sidebar space
    - Wrap application with AudioProvider context
    - Integrate CustomScrollbar and AudioToggle components
    - Remove React import for React 19 JSX transform
    - Update flex layout structure for new navigation

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import CustomScrollbar from './CustomScrollbar';
+import AudioToggle from './AudioToggle';
+import { AudioProvider } from './AudioToggle';
 import { ReactLenis } from 'lenis/react';
 
 const Layout = () => {
   return (
-    <ReactLenis root>
-      {/* Noticeable bg - future infrastructure */}
-      <div className='bg-transparent min-h-screen flex flex-col'>
-        <Navbar />
-        <main className='grow'>
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    </ReactLenis>
+    <AudioProvider>
+      <ReactLenis root>
+        <div className="min-h-screen flex" style={{ backgroundColor: 'var(--color-bg-body)' }}>
+          <Navbar />
+          
+          <main className="flex-1 ml-16">
+            <Outlet />
+            <Footer />
+          </main>
+          
+          <CustomScrollbar />
+          <AudioToggle />
+        </div>
+      </ReactLenis>
+    </AudioProvider>
   );
 };
 
