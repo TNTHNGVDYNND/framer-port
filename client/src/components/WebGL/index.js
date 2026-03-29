34f088c68ab1ca24a22610d3151591f15ea6bd15
TVATDCI <tvatdci@gmail.com>
2026-03-29 23:52:11 +0200

    updated:

--- client/src/components/WebGL/index.js
+++ client/src/components/WebGL/index.js
@@ -1,11 +1,18 @@
+/*
+ * @deprecated - WebGL components are not currently in use
+ * Replaced with CSS-based background effects for better performance
+ * Files preserved for future refactoring or potential WebGL re-implementation
+ */
+
 // WebGL Components - Barrel export
-export { default as WebGLBackground } from './WebGLBackground';
-export { default as BackgroundMaterial } from './BackgroundMaterial';
-export { 
-  FluidBackgroundMaterial, 
-  FluidFragmentShader, 
-  FluidVertexShader 
-} from './FluidShader';
+// NOTE: These exports are commented out - components not currently imported anywhere
+// export { default as WebGLBackground } from './WebGLBackground';
+// export { default as BackgroundMaterial } from './BackgroundMaterial';
+// export { 
+//   FluidBackgroundMaterial, 
+//   FluidFragmentShader, 
+//   FluidVertexShader 
+// } from './FluidShader';
 
 // Hooks (also exported from their own directory)
 export { default as useMousePosition } from '../../hooks/useMousePosition';
